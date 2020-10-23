90e1853ff81354948c1068349a472c1aa711a339
Kim Van de Casteele <Kinkyk013@gmail.com>
2020-10-23 11:58:11 +0200

    Setup Project: adding dependencies

--- src/test/java/hangman/TestHangman.java
+++ src/test/java/hangman/TestHangman.java
@@ -3,12 +3,14 @@ package hangman;
 import javafx.beans.binding.BooleanExpression;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TestHangman {
 
     @Test
     public void testNoGuess_A() {
         Hangman hangman = new Hangman("A");
-        assert(hangman.hint()).equals("_");
+        assertThat(hangman.hint()).isEqualTo("_");
     }
 
 }
